4c4db6a574d4e5f939ecd45a07eed7702801961d
CuttleFish <b9804320975@gmail.com>
2024-02-29 14:16:44 +0800

    init

---
+++ TaiwanHistoricalWeatherData-10Year/src/main/java/com/taiwanhistoricalweatherdata10year/TaiwanHistoricalWeatherData10YearApplication.java
@@ -0,0 +1,13 @@
+package com.taiwanhistoricalweatherdata10year;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaiwanHistoricalWeatherData10YearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaiwanHistoricalWeatherData10YearApplication.class, args);
+    }
+
+}
---
+++ TaiwanHistoricalWeatherData-10Year/src/test/java/com/taiwanhistoricalweatherdata10year/TaiwanHistoricalWeatherData10YearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taiwanhistoricalweatherdata10year;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaiwanHistoricalWeatherData10YearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
